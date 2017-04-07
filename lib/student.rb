951ba1d0f991386bb349d1dbe968e9848eaf0100
Mike Merin <mikemerinweather@gmail.com>
2017-04-07 20:04:17 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative './user.rb'
+
+class Student<User
+
+attr_accessor :first_name, :last_name, :knowledge
+
+def initialize
+  @first_name = "Steve"
+  @last_name = "Jobs"
+  @knowledge = []
+end
+
+def learn(string)
+  @knowledge << string
+end
+
+def knowledge
+  @knowledge
+end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class User
+
+attr_accessor :first_name, :last_name
+
+def User
+  @first_name = Tracy
+  @last_name = Morgan
+end
+
+end
